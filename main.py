8f8a7755fe049ea8eb163aa43a8bc84f4950d26e
Conrad Krantz <conradkrantz@gmail.com>
2023-09-23 22:44:20 -0500

    able to save a date from the user

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #import date and time
 from datetime import datetime
-print("What is your birthday?")
-bday = input()
-print(bday)
+
+#getting basic information
+print("What Year where you born?")
+Year = int(input())
+print("What MOnth were you born?")
+Month = int(input())
+print("What Day were you born?")
+Day = int(input())
+
+#calculating date
+Birthday = datetime(Year, Month, Day)
+print(Birthday)
