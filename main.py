6bbfc653919bb587c295e50f00bcf208b17852bd
Conrad Krantz <conradkrantz@gmail.com>
2023-09-24 20:54:29 -0500

    Have added month logic

--- main.py
+++ main.py
@@ -7,12 +7,34 @@ Year = int(input())
 
 print("What Month were you born?")
 Month = input().lower()
-if Month == "test":
+if Month == "january":
     Month = 1
+elif Month == "february":
+    Month = 2
+elif Month == "march":
+    Month = 3
+elif Month == "april":
+    Month = 4
+elif Month == "may":
+    Month = 5
+elif Month == "june":
+    Month = 6
+elif Month == "july":
+    Month = 7
+elif Month == "august":
+    Month = 8
+elif Month == "september":
+    Month = 9
+elif Month == "october":
+    Month = 10
+elif Month == "november":
+    Month = 11
+elif Month == "december":
+    Month = 12
 
 print("What Day were you born?")
 Day = int(input())
 
 #calculating date
 Birthday = datetime(Year, Month, Day)
-print(Birthday)
+print("Your birthday is " + str(Birthday))
